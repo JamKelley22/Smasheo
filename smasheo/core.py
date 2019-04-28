617dff781279bce70bb769e003622b1543286427
= <brianguidarini@gmail.com>
2019-04-27 23:35:45 -0500

    Cleaned up the mess I made in core.py

--- smasheo/core.py
+++ smasheo/core.py
@@ -13,7 +13,54 @@ import audioProcessing as ap
 import stockDetection
 import playerDetection as pd
 
+def handleAttacks(upSmashes, timeStamp, hammerArea, dedeOnPlat, movesOnHold, doDrawAttack, labelFrame, attackFrame):
+	if (len(upSmashes) > 0 and timeStamp >= upSmashes[0]):
+		time = upSmashes.pop(0)
+		if (hammerArea >= UP_HAMM_AREA_MIN and dedeOnPlat):
+			doDrawAttacks = True
+			attackFrame = count
+			#print timeStamp, upSmashes[0]
+		else:
+			movesOnHold.append([0, time, count + 10])
+	if (doDrawAttack):
+		labelFrame = pd.drawAttack(labelFrame)
+		if (count - attackFrame >= 30):
+			doDrawAttack = False
+	removeLater = []
+	for i in range(0, len(movesOnHold)):
+		held = movesOnHold[i]
+		if (hammerArea >= UP_HAMM_AREA_MIN and dedeOnPlat):
+			doDrawAttack = True
+			attackFrame = count
+		elif count > held[2]:
+			removeLater.append(movesOnHold[i])
+	for i in range(0, len(removeLater)):
+		movesOnHold.remove(removeLater[i])
+
+def trackObjects(frame, hammerAvg):
+	dedePos, dMask = pd.findDedede(frame)
+	kirbyPos = pd.findKirby(frame)
+	gordoPos = pd.findGordo(frame)
+	labelFrame = pd.drawLabel(frame, dedePos[4], dedePos[1], dedePos[0], (0, 0, 255))
+	labelFrame = pd.drawLabel(labelFrame, kirbyPos[4], kirbyPos[1], kirbyPos[0], (0, 0, 255))
+	hammerPos = pd.findHammer(labelFrame)
+	labelFrame = pd.drawLabel(labelFrame, hammerPos[4], hammerPos[1], hammerPos[0], (0, 0, 255))
+	labelFrame = pd.drawLabel(labelFrame, gordoPos[4], gordoPos[1], gordoPos[0], (0, 0, 255))
+	hammerAvg.insert((hammerPos[0], hammerPos[1]))
+	return labelFrame, dedePos, kirbyPos, hammerPos
+
+def trackStock(curStockD, curStockK, labelFrame, count):
+	frmStockD = stockDetection.getDededeStock(labelFrame)
+	frmStockK = stockDetection.getKirbyStock(labelFrame)
+	if (frmStockD != -1 and frmStockD <= curStockD) or count == 0:
+		curStockD = frmStockD
+	if (frmStockK != -1 and frmStockK <= curStockK) or count == 0:
+		curStockK = frmStockK
+	return curStockD, curStockK
+
 def main():
+	tracker = []
+
 	ap = argparse.ArgumentParser()
 	ap.add_argument("-v", "--video")
 	args = vars(ap.parse_args())
@@ -48,61 +95,19 @@ def main():
 		print(damage.whatsYourDamage(frame,frame_width,frame_height))
 		cv2.waitKey(17)
 		timeStamp = count * 16.6667
-		bw = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-		dummy, bw = cv2.threshold(bw, 100, 255, cv2.THRESH_BINARY)
-		dedePos, dMask = pd.findDedede(frame)
-		kirbyPos = pd.findKirby(frame)
-		gordoPos = pd.findGordo(frame)
-		labelFrame = pd.drawLabel(frame, dedePos[4], dedePos[1], dedePos[0], (0, 0, 255))
-		labelFrame = pd.drawLabel(labelFrame, kirbyPos[4], kirbyPos[1], kirbyPos[0], (0, 0, 255))
-		hammerPos = pd.findHammer(labelFrame)
-		labelFrame = pd.drawLabel(labelFrame, hammerPos[4], hammerPos[1], hammerPos[0], (0, 0, 255))
-		labelFrame = pd.drawLabel(labelFrame, gordoPos[4], gordoPos[1], gordoPos[0], (0, 0, 255))
-		hammerAvg.insert((hammerPos[0], hammerPos[1]))
-		displacement = hammerAvg.getDisplacement()
-		dX = displacement[0]
-		dY = displacement[1]
 
-		frmStockD = stockDetection.getDededeStock(labelFrame)
-		frmStockK = stockDetection.getKirbyStock(labelFrame)
-		if (frmStockD != -1 and frmStockD <= curStockD) or count == 0:
-			curStockD = frmStockD
-		if (frmStockK != -1 and frmStockK <= curStockK) or count == 0:
-			curStockK = frmStockK
-		print curStockD, curStockK
+		labelFrame, dedePos, hammerPos, kirbyPos = trackObjects(frame, hammerAvg)
+		curStockD, curStockK = trackStock(curStockD, curStockK, labelFrame, count)
 		hammerArea = hammerAvg.area()
-		for i in range(0, len(hammerAvg.getSet())):
-			pd.drawPoint(labelFrame, hammerAvg.getSet()[i])
+		dedeOnPlat = pd.onPlatform(dedePos)
+		handleAttacks(upSmashes, timeStamp, hammerArea, dedeOnPlat, movesOnHold, doDrawAttack, labelFrame, attackFrame)
+		
+		# for i in range(0, len(hammerAvg.getSet())):
+		# 	pd.drawPoint(labelFrame, hammerAvg.getSet()[i])
 		#dedeFrame = cv2.cvtColor(dedeFrame, cv2.COLOR_BGR2HSV)
 
-		dedeOnPlat = pd.onPlatform(dedePos)
-		#print movesOnHold, hammerArea, dedeOnPlat
-		if (len(upSmashes) > 0 and timeStamp >= upSmashes[0]):
-			time = upSmashes.pop(0)
-			if (hammerArea >= UP_HAMM_AREA_MIN and dedeOnPlat):
-				doDrawAttack = True
-				attackFrame = count
-				#print timeStamp, upSmashes[0]
-			else:
-				movesOnHold.append([0, time, count + 10])
-		if (doDrawAttack):
-			labelFrame = pd.drawAttack(labelFrame)
-			if (count - attackFrame >= 30):
-				doDrawAttack = False
-
-		removeLater = []
-		for i in range(0, len(movesOnHold)):
-			held = movesOnHold[i]
-			if (hammerArea >= UP_HAMM_AREA_MIN and dedeOnPlat):
-				doDrawAttack = True
-				attackFrame = count
-			elif count > held[2]:
-				removeLater.append(movesOnHold[i])
-		for i in range(i, len(removeLater)):
-			movesOnHold.remove(removeLater[i])
 
 		cv2.imshow("Video", labelFrame)
-		cv2.imshow("bw", dMask)
 
 		count += 1
 		#writer.write(labelFrame)
