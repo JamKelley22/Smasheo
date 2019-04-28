ad73e055f151b2b0278762802d55426ec88e5cfa
Jameel Kelley <jkelley@iastate.edu>
2019-04-28 01:38:25 -0500

    Was messing with Tkinter GUI

--- smasheo/core.py
+++ smasheo/core.py
@@ -3,7 +3,9 @@ import numpy as np
 import imutils
 import argparse
 from collections import deque
-import damage
+import Tkinter as tk
+import threading
+import playerInfo
 
 ap = argparse.ArgumentParser()
 ap.add_argument("-v", "--video")
@@ -16,20 +18,36 @@ fps = int(vs.get(5))
 fourcc = cv2.VideoWriter_fourcc('M','J','P','G')
 #out = cv2.VideoWriter('outpy.avi',fourcc, fps, (frame_width,frame_height),1)
 
-while True:
-	frame = vs.read()
-	frame = frame[1]
-	if frame is None:
-		break
+def run():
+	def callback():
+		while True:
+			frame = vs.read()
+			frame = frame[1]
+			if frame is None:
+				break
 
-	print(damage.whatsYourDamage(frame,frame_width,frame_height))
 
-	#out.write(frame)
-	key = cv2.waitKey(1) & 0xFF
 
-	if key == ord("q"):
-		break
+			#print(playerInfo.whatsYourDamage(frame,frame_width,frame_height))
+			#playerInfo.getYourLifeOverMine(frame,frame_width,frame_height)
 
-vs.release()
-#out.release()
-cv2.destroyAllWindows()
+			#out.write(frame)
+			key = cv2.waitKey(1) & 0xFF
+
+			if key == ord("q"):
+				break
+
+		vs.release()
+		#out.release()
+		cv2.destroyAllWindows()
+	t = threading.Thread(target=callback)
+	t.start()
+	return t;
+
+
+r = tk.Tk()
+r.title('Smasheo')
+
+button = tk.Button(r, text='Run', width=25, command= run())
+button.pack()
+r.mainloop()
